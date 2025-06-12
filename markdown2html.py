3f314924973dde439da2b41a0fb55094bb555963
Zerufael <zerufaelaemero@gmail.com>
2025-06-12 10:46:25 -0400

    edit

--- markdown2html.py
+++ markdown2html.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
+"""
+markdown2html.py - Script to check Markdown input file and output HTML filename.
+"""
 import sys
 import os
 
-# Check for the correct number of arguments (2 file names = 3 argv items total)
-if len(sys.argv) < 3:
-    print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
-    sys.exit(1)
+def main():
+    if len(sys.argv) < 3:
+        print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
+        sys.exit(1)
+    if not os.path.exists(sys.argv[1]):
+        print(f"Missing {sys.argv[1]}", file=sys.stderr)
+        sys.exit(1)
+    sys.exit(0)
 
-# Check if the input Markdown file exists
-if not os.path.exists(sys.argv[1]):
-    print(f"Missing {sys.argv[1]}", file=sys.stderr)
-    sys.exit(1)
-
-# All good
-sys.exit(0)
+if __name__ == "__main__":
+    main()
